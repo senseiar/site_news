9da280c81af412bd88424101cc15d23ab340b3af
Andrey Ratkin <aratkin77@gmail.com>
2018-06-12 14:49:37 +0300

    edited gitignore

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=news_mod4',
     'username' => 'root',
-    'password' => 'prodex',
+    'password' => '',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
